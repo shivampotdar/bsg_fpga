13eeecb5186aaa73faab5dcc77652b62bd918559
Paul Gao <gaozihou@uw.edu>
2020-12-07 19:47:29 -0800

    update Makefile for FPGA programming

--- vcu128/script/program_fpga.tcl
+++ vcu128/script/program_fpga.tcl
@@ -9,7 +9,7 @@ connect_hw_server
 open_hw_target
 
 # set programming file
-set_property PROGRAM.FILE ./vcu128_test/vcu128_test.runs/impl_1/design_1_wrapper.bit [get_hw_devices xcvu37p_0]
+set_property PROGRAM.FILE ./design_1_wrapper.bit [get_hw_devices xcvu37p_0]
 
 # program FPGA
 program_hw_devices -verbose [get_hw_devices xcvu37p_0]
